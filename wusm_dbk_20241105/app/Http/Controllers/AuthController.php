b5979ec21e6d32c4d3a9d3df436c2e254deaf6cb
Wilman Uriel Salmeron Martinez <mrwilman053@gmail.com>
2024-11-07 16:40:37 -0600

    arreglo de codigos

--- wusm_dbk_20241105/app/Http/Controllers/AuthController.php
+++ wusm_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -2,25 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Hash;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
-    public function register(Request $request) {
-        $validate = $request->validate(rules: [
-            'name'=>['required', 'string','max:255'],
-            'email'=>['required','string','email','max:255','unique:users'],
-            'password'=>['required', 'string','min:8','max:20'],
-
+    public function register(Request $request): void{
+        $validatedData = $request->validate(rules: [
+        'name'=>['required','string','max:255'],
+        'email'=>['required','string','email','max:255','unique:users'],
+        'password'=>['required','string','min:8','max:20'],
         ]);
 
-        $user = User::create(Attributes:[
+        $user = User::create(attributes:[
             'name'=> $validatedData['name'],
             'email'=> $validatedData['email'],
             'password'=> Hash::make(value: $validatedData['password']),
+        ]);
 
-        ])
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(data: [
+            "success"=> true,
+            "errors"=>[
+                "code"=>0,
+                "msg"=>""
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type" => "Bearer"
+            ],
+            "msg"=>"Usuario creado satisfactoriamente",
+            "count"=>1
+        ]);
     }
+
 }
